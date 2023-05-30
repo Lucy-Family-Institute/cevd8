2c754960cb69e2f52fdf39d831d5402ce495e321
Pedro Ballesteros <pedro.ballesteros@comisiondelaverdad.co>
2023-05-30 17:03:09 -0500

    Fix copy URL in modals

--- web/themes/custom/cevtheme/botonera/js/arreglos.js
+++ web/themes/custom/cevtheme/botonera/js/arreglos.js
@@ -118,3 +118,13 @@ jQuery("body").on("click", '.cambiar_letra_pequenia', function (e) {
   }
 })
 
+function copiarURL(texto) {
+  // Get the text field
+  var copyText = texto;
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
