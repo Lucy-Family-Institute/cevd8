77d1b9a9ce33bf11060c4284a5e0f9300b09d018
Pedro Ballesteros <pedro.ballesteros@comisiondelaverdad.co>
2023-06-14 16:04:07 -0500

    Visual changes in Lo que sigue - Comité de seguimiento section

--- web/themes/custom/cevtheme/comite-de-seguimiento/js/script-cds.js
+++ web/themes/custom/cevtheme/comite-de-seguimiento/js/script-cds.js
@@ -1,81 +1,40 @@
 jQuery(window).on('load resize', function(){
 
-
-
     var width = jQuery(window).width();
     if((width) >= 991){
- 
- 
- 
      jQuery('.card-first .card-principal').on('click',function(){
- 
          green();
- 
      })
- 
-     // jQuery('.information-scale-2').on('click',function(){
- 
-     //     home();
- 
-     // })
  
      jQuery('.card-second .card-principal').on('click',function(){
- 
          blue();
- 
      })
  
-     // jQuery('.card-second .card-blue').on('click',function(){
- 
-     //     home();
- 
-     // })
- 
      jQuery('.item-container-principal-3.card-principal').on('click',function(){
- 
-         red();
- 
+         if(jQuery(this).hasClass('card-close') == false){
+             console.log(jQuery(this).attr('class'));
+             red();
+         }
      })
  
      jQuery('.arrow-home').on('click', function(){
- 
          home()
- 
      })
  
      jQuery('.card-third .card-red').on('click',function(){
- 
- 
- 
-         // jQuery('.card-red').removeClass('card-show-100');
-         home();
+         console.log(jQuery(this).attr('class'));
+         //home();
      })
  
     } else{
- 
- 
          jQuery('.card-first .card-principal').on('click',function(){
- 
              green_mobile();
- 
          })
- 
-         // jQuery('.card-first .card-green').on('click',function(){
-         //     jQuery('.card-green').hide();
-         //     alert('holii')
-         //     jQuery('.card-principal').show();
-         // })
  
          jQuery('.card-second .card-principal').on('click',function(){
              jQuery('.card-principal').hide();
              jQuery('.card-blue').show(); 
          })
-         // jQuery('.card-second .card-blue').on('click',function(){
-         //     jQuery('.card-blue').hide();
-         //     jQuery('.card-principal').show();
-         // })
- 
- 
  
          jQuery('.card-third .card-principal').on('click',function(){
              jQuery('.card-principal').hide();
@@ -89,7 +48,7 @@ jQuery(window).on('load resize', function(){
          })
            // navegación superior mobile
  
-           jQuery('.card-first .card-principal , .card-second .card-principal , .card-third .card-principal').on('click', function(){
+         jQuery('.card-first .card-principal , .card-second .card-principal , .card-third .card-principal').on('click', function(){
              jQuery(window).scrollTop(0);
              jQuery('.wrapper').css('margin-top', '45px')
              jQuery('.wrapper .information-principal').css('padding', '0')
@@ -108,45 +67,28 @@ jQuery(window).on('load resize', function(){
          })
  
          jQuery('.card-third').on('click', function(){
- 
              jQuery('.section-links button:nth-child(3)').addClass('current-showing')
- 
              jQuery('.row-content').addClass('active-red')
- 
          })
  
          jQuery('.section-links .green-btn').on('click',function(){
- 
              green_mobile();
- 
              jQuery('.row-content').addClass('active-green')
- 
              jQuery('.componen').removeClass('d-none')
- 
              jQuery('.row-content').removeClass('active-red , active-blue')
- 
          })
  
          jQuery('.section-links .blue-btn , .componen').on('click',function(){
- 
              blue_mobile();
- 
              jQuery('.row-content').addClass('active-blue')
- 
              jQuery('.row-content').removeClass('active-red , active-green')
- 
              jQuery('.componen').removeClass('d-none')
- 
          })
  
          jQuery('.section-links .red-btn , .publicacion').on('click',function(){
- 
              red_mobile();
- 
              jQuery('.row-content').removeClass('active-blue , active-green')
- 
              jQuery('.row-content').addClass('active-red')
- 
          })
  
          jQuery('.publicacion , .componen').on('click', function(){
@@ -156,101 +98,56 @@ jQuery(window).on('load resize', function(){
          })
  
          jQuery('.publicacion').on('click', function(){
- 
              jQuery('.section-links .red-btn').addClass('current-showing')
- 
          })
  
-         jQuery('.section-links button').on('click',function(){
-             
+         jQuery('.section-links button').on('click',function(){         
              if(jQuery(this).hasClass('current-showing')){
- 
                  jQuery('.card-green , .card-blue , .card-red').hide();
- 
                  jQuery('.card-principal').show();
- 
                  jQuery('.section-links button').removeClass('current-showing')
- 
                  jQuery('.section-links').removeClass('show-sections-links')
- 
                  jQuery('.wrapper').css('margin-top', '')
                  jQuery('.wrapper .information-principal').css('padding', '')
- 
                  jQuery('.row-content').removeClass('active-blue , active-red , active-green')
- 
              }else{
                  jQuery(window).scrollTop(0);
- 
                  jQuery('.section-links button').removeClass('current-showing')
- 
                  jQuery(this).addClass('current-showing')
- 
              }
- 
          })
  
          // navegación superior mobile
- 
- 
- 
          jQuery('.item-container-principal.card-principal , .item-container-principal-2.card-principal , .item-container-principal-3.card-principal').on('click', function(){
- 
              jQuery('.section-links').toggleClass('show-sections-links')
- 
              jQuery('.section-links button').removeClass('d-none')
- 
          })
  
          jQuery('.item-container-principal-2').on('click', function(){
- 
              jQuery('.section-links button:nth-child(3)').addClass('d-none')
- 
          })
  
          jQuery('.item-container-principal-3').on('click', function(){
- 
              jQuery('.componen').addClass('d-none')
- 
              jQuery('.section-links button').removeClass('d-none')
- 
          })
  
          jQuery('.item-container-principal').on('click', function(){
- 
              jQuery('.section-links button:nth-child(2) , .section-links button:nth-child(3)').addClass('d-none')
- 
          })
     }
- 
- 
- 
  });
  
-  
  
  
- 
- 
- 
- 
- 
- 
- 
- /*jQuery(window).on('load resize', function() {
- 
+ jQuery(window).on('load resize', function() {
      width = jQuery(window).width();
- 
      if (width <= 991) {
- 
          jQuery(window).on("resize", function() {
- 
              location.reload();
- 
          });
-       
      }
- 
- })*/
+ })
  
  
  
@@ -428,3 +325,11 @@ jQuery(window).on('load resize', function(){
      jQuery('.section-links button:nth-child(3), .section-links button:nth-child(2)').removeClass('d-none')
  
  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
